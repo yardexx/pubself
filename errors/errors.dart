f437960cbb1b1462dcc5078949ea03c2f852af21
Jaroslav Novotny <novotnyj912@gmail.com>
2022-09-26 16:08:17 +0200

    feat: Added error model

---
+++ errors/errors.dart
@@ -0,0 +1,2 @@
+export 'pub_error.dart';
+export 'response_error.dart';
---
+++ errors/pub_error.dart
@@ -0,0 +1,28 @@
+// TODO(Yardex): Remove when implemented and documented
+// ignore_for_file: public_member_api_docs
+
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:meta/meta.dart';
+
+import 'errors.dart';
+
+part 'pub_error.g.dart';
+
+@immutable
+@JsonSerializable(explicitToJson: true)
+class PubError extends Equatable {
+  const PubError(this.error);
+
+  final ResponseError error;
+
+  static PubError fromJson(Map<String, dynamic> json) =>
+      _$PubErrorFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PubErrorToJson(this);
+
+
+  @override
+  List<Object?> get props => [error];
+  
+}
