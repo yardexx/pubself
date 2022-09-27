2debfc09f650d45f740facf16f874f1283c23231
Jaroslav Novotny <novotnyj912@gmail.com>
2022-09-27 11:03:52 +0200

    feat: Handling GET request

--- routes/api/packages/[package_name].dart
+++ routes/api/packages/[package_name].dart
@@ -1,5 +1,19 @@
-import 'package:dart_frog/dart_frog.dart';
+import 'dart:async';
+import 'dart:io';
 
-Response onRequest(RequestContext context, String name) {
-  return Response(body: 'Package: $name');
+import 'package:dart_frog/dart_frog.dart';
+import 'package:package_storage/package_storage.dart';
+
+FutureOr<Response> onRequest(RequestContext context, String packageName) {
+  if(context.request.method == HttpMethod.get){
+    return _get(context, packageName);
+  }
+
+  return Response(statusCode: HttpStatus.methodNotAllowed);
+}
+
+Future<Response> _get(RequestContext context, String packageName) async {
+  final storage = context.read<PackageStorage>();
+  final package = await storage.fetch(packageName);
+  return Response.json(body: package);
 }
