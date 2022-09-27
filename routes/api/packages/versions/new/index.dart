e0f31f798d7aad7618ef4f05580baecf345c52ed
Jaroslav Novotny <novotnyj912@gmail.com>
2022-09-27 11:04:53 +0200

    feat: Handling requests

--- routes/api/packages/versions/new/index.dart
+++ routes/api/packages/versions/new/index.dart
@@ -1,5 +1,15 @@
+import 'dart:io';
+
 import 'package:dart_frog/dart_frog.dart';
 
+import '../../../../../errors/errors.dart';
+
 Response onRequest(RequestContext context) {
-  return Response(body: 'New package');
+  return Response.json(
+    statusCode: HttpStatus.badRequest,
+    body: PubError.withData(
+      code: 'not-implemented',
+      message: 'This endpoint has not been implemented yet. Sorry 😅',
+    ),
+  );
 }
